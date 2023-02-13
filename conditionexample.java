ec7c038f0ad5d2ace8390efa60543a0b79778254
sumit <123495189+Sumitdongre8799@users.noreply.github.com>
2023-02-13 23:18:30 +0530

    Add files via upload

---
+++ conditionexample.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.util.Scanner.*;
+
+public class conditionexample {
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+        int button = sc.nextInt();
+
+        switch (button) {
+            case 1:
+                System.out.println(" Hello ");
+                break;
+            case 2:
+                System.out.println("Namaste");
+                break;
+            case 3:
+                System.out.println("Bonjour");
+                break;
+            default:
+                System.out.println("Invalide Button");
+
+        }
+
+    }
+}
